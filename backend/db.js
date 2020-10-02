cf784f1a1220740a366e092b572eda44480b0c73
kimyounghoon <choosey_@.naver.com>
2020-10-02 22:48:12 +0900

    server.js fixed

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql");
+
 const pool = mysql.createPool({
   connectionLimit: 10,
   host: process.env.MYSQL_HOST,
--- backend/server.js
+++ backend/server.js
@@ -7,33 +7,40 @@ const app = express();
 app.use(bodyParser.json());
 
 db.pool.query(
-	`Create table lists()
-    Id INTERGER AUTO_INCREMENT,
-    Value Text,
-    Prmary Key (id)
-`,
-	(err, res, fileds) => {
-		console.log(res);
-	}
+  `CREATE TABLE lists (
+    id INTEGER AUTO_INCREMENT,
+    value TEXT, 
+    PRIMARY KEY (id)
+)`,
+  (err, results, fileds) => {
+    console.log("results", results);
+  }
 );
 
-app.get("/api/values", (req, res) => {
-	db.pool.query("SELECT * FROM lists", (err, results, fileds) => {
-		if (err) return res.status(500).send(err);
-		else return res.json(results);
-	});
+app.get("/api/hi", function (req, res) {
+  //데이테베이스에서 모든 정보 가져오기
+  res.status(200).send("good");
 });
 
-app.post("api/value", (req, res) => {
-	db.pool.query(
-		`INSERT  INTO lists ${req.body.value}`,
-		(err, results, fields) => {
-			if (err) return res.status(500), send(err);
-			else return res.json({ success: true, value: req.body.value });
-		}
-	);
+app.get("/api/values", function (req, res) {
+  //데이테베이스에서 모든 정보 가져오기
+  db.pool.query("SELECT * FROM lists;", (err, results, fileds) => {
+    if (err) return res.status(500).send(err);
+    else return res.json(results);
+  });
+});
+
+app.post("/api/value", function (req, res, next) {
+  //데이터베이스에 값 넣어주기
+  db.pool.query(
+    `INSERT INTO lists (value) VALUES("${req.body.value}")`,
+    (err, results, fileds) => {
+      if (err) return res.status(500).send(err);
+      else return res.json({ success: true, value: req.body.value });
+    }
+  );
 });
 
 app.listen(5000, () => {
-	console.log("5000번 포트에서 시작되었습니다");
+  console.log("5000번 포트에서 시작되었습니다");
 });
