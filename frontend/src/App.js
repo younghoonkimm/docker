30bb47d9fd06ac516fd4c5b53b3f8ae9e7ead2a5
kimyounghoon <choosey_@.naver.com>
2020-10-15 10:14:57 +0900

    frontend layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import axios from 'axios';
 
 function App() {
   useEffect(() => {
-    axios.get("/api/hi").then((response) => {
-      console.log("response", response);
+    axios.get('/api/hi').then((response) => {
+      console.log('response', response);
     });
   }, []);
 
   useEffect(() => {
-    axios.get("/api/values").then((response) => {
-      console.log("response", response);
+    axios.get('/api/values').then((response) => {
+      console.log('response', response);
       setLists(response.data);
     });
   }, []);
 
   const [lists, setLists] = useState([]);
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState('');
 
   const changeHandler = (event) => {
     setValue(event.currentTarget.value);
@@ -27,13 +26,13 @@ function App() {
   const submitHandler = (event) => {
     event.preventDefault();
 
-    axios.post("/api/value", { value: value }).then((response) => {
+    axios.post('/api/value', { value: value }).then((response) => {
       if (response.data.success) {
-        console.log("response", response.data);
+        console.log('response', response.data);
         setLists([...lists, response.data]);
-        setValue("");
+        setValue('');
       } else {
-        alert("값을 DB에 넣는데 실패했습니다.");
+        alert('값을 DB에 넣는데 실패했습니다.');
       }
     });
   };
@@ -41,8 +40,7 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <div className='container'>
+        {/* <div className='container'>
           {lists &&
             lists.map((list, index) => <li key={index}>{list.value} </li>)}
           <br />
@@ -56,7 +54,7 @@ function App() {
             />
             <button type='submit'>확인.</button>
           </form>
-        </div>
+        </div> */}
       </header>
     </div>
   );
